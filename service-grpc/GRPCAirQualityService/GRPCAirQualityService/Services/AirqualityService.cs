1128d08f73681151faa6c60d95bf09fd7eb52eb5
Veljko Cocojevic <veljkococa@gmail.com>
2026-05-12 15:59:56 +0200

    Initial commit: Baza i gRPC servis

---
+++ service-grpc/GRPCAirQualityService/GRPCAirQualityService/Services/AirqualityService.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Core;
+using GRPCAirQualityService.Protos;
+
+namespace GRPCAirQualityService.Services
+{
+    public class AirqualityService : AirQualityService.AirQualityServiceBase
+    {
+        /*public override Task<AirQualityService> AddSensorData(SensorDataRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new IngestResponse
+            {
+                Success = true
+            });
+        }*/
+    }
+}
